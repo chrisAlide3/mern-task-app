d345f5cf1f4ffda14a15740b8e0da21f5648c788
chrisAlide3 <christian.bracher@gmail.com>
2023-05-23 10:00:55 +0200

    project setup

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+// Routes
+
+// Starting server on port 5000
+app.listen("5000", () => {
+  console.log("Server running on port 5000");
+});
